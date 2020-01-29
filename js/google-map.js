93a07c24b93ccdcc4769f73dd2ca13708d26088e
Strnad Jan <strnad@avast.com>
2020-01-29 21:09:16 +0100

    Initial

---
+++ js/google-map.js
@@ -0,0 +1,35 @@
+
+var google;
+
+function init() {
+    // Basic options for a simple Google Map
+    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+    // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
+    var myLatlng = new google.maps.LatLng(50.067955, 17.091437);
+
+    var mapOptions = {
+        // How zoomed in you want the map to start at (always required)
+        zoom: 15,
+
+        // The latitude and longitude to center the map (always required)
+        center: myLatlng,
+
+        // How you would like to style the map. 
+        scrollwheel: false,
+    };
+
+    
+
+    // Get the HTML DOM element that will contain your map 
+    // We are using a div with id="map" seen below in the <body>
+    var mapElement = document.getElementById('map');
+
+    // Create the Google Map using out element and options defined above
+    var map = new google.maps.Map(mapElement, mapOptions);
+    
+    new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+    });
+}
+google.maps.event.addDomListener(window, 'load', init);
